170a4921a497f8e502da34fc6243e60fea9cc62c
Антон Ивашкин <aivash.tech@gmail.com>
2026-03-14 15:24:51 +0300

    refactor: add index to g_number in orders table

--- database/migrations/2026_03_14_121125_create_orders_table.php
+++ database/migrations/2026_03_14_121125_create_orders_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
 
-            $table->string('g_number')->nullable();
+            $table->string('g_number')->nullable()->index();
             $table->timestamp('date')->nullable();
 
             $table->string('supplier_article')->nullable();
