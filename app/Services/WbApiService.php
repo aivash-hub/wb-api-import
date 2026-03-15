a073342ea335cbe2c6f7c8bfac8e476962435a98
Антон Ивашкин <aivash.tech@gmail.com>
2026-03-15 20:38:11 +0300

    refactor: add retry and timeout to WB API client

--- app/Services/WbApiService.php
+++ app/Services/WbApiService.php
@@ -10,19 +10,21 @@ class WbApiService
     private string $baseUrl = 'http://109.73.206.144:6969/api';
     private string $apiKey = 'E6kUTYrYwZq2tN4QEtyzsbEBk3ie';
 
-    public function getOrders(string $dateFrom,
-                              string $dateTo,
-                              int $page = 1,
-                              int $limit = 500
-    ): array
-    {
-        $response = Http::get("{$this->baseUrl}/orders", [
-            'page' => $page,
-            'limit' => $limit,
-            'key' => $this->apiKey,
-            'dateFrom' => $dateFrom,
-            'dateTo' => $dateTo,
-        ]);
+    public function getOrders(
+        string $dateFrom,
+        string $dateTo,
+        int $page = 1,
+        int $limit = 500
+    ): array {
+        $response = Http::retry(3, 100)
+            ->timeout(10)
+            ->get("{$this->baseUrl}/orders", [
+                'page' => $page,
+                'limit' => $limit,
+                'key' => $this->apiKey,
+                'dateFrom' => $dateFrom,
+                'dateTo' => $dateTo,
+            ]);
 
         return $response->json();
     }
